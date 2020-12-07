c5f101a775690a308ccfe546d05ebbdf23b4f23f
abumeis <37344104+abumeis@users.noreply.github.com>
2020-12-07 17:07:20 +0100

    avec l'aide d'Exv

---
+++ jour01.js
@@ -0,0 +1,7 @@
+console.log("Hello World !");
+var test = "My name is Ahmed";
+console.log(test);
+var name = "Ahmed";
+console.log("Nice to meet you " + name);
+var testLength = "I'm very long !";
+console.log(testLength.length);
