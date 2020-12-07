eaef469eec6975807acafa9290b061222c93afb1
abumeis <37344104+abumeis@users.noreply.github.com>
2020-12-07 18:37:10 +0100

    Split added

--- jour01.js
+++ jour01.js
@@ -1,14 +1,35 @@
+// Hello world:
 console.log("Hello World !");
+
+
+// testing my name :
 var test = "My name is Ahmed";
 console.log(test);
+
+
+// testing my name again :
 var name = "Ahmed";
 console.log("Nice to meet you " + name);
+
+// testing the lenght :
 var testLength = "I'm very long !";
 console.log(testLength.length);
+
+
+// replacement words:
 var food = "croissant is meh";
 console.log(food.replace("meh", "so good"));
+
+
+// testing upper and lower case:
 var basic = "This is Cool";
+console.log(basic);
 var basicUp = "basic";
 console.log(basicUp.toUpperCase());
 var basicDown = "basic";
 console.log(basicUp.toLocaleLowerCase());
+
+// Split words :
+var word = "banana";
+var letters = word.split('');
+console.log(letters);
