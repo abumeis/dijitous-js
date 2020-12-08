f4e720d8e8fc9d24054463a7a986e8443bb74bd2
abumeis <37344104+abumeis@users.noreply.github.com>
2020-12-08 14:25:04 +0100

    first 4 exercise

---
+++ jour02.js
@@ -0,0 +1,31 @@
+// ## 01 - Number:
+
+var integer = 102;
+var float = 13.9;
+
+console.log(integer, float);
+
+
+
+//## 02 - Convert
+
+var basic = 34;
+var stringified = basic.toString();
+console.log(stringified);
+
+//## 03 - Round
+
+var num = 1.5;
+var rounded = Math.round(num);
+console.log(rounded);
+
+//## 04 - Arithmétique
+
+var test = 12;
+var bis = 5;
+console.log(test + bis);
+console.log(test - bis);
+console.log(test * bis);
+console.log(test / bis);
+console.log(test ** bis);
+console.log(test % bis);
