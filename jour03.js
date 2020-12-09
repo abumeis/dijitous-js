38db447b420a1f790668b54aa8aa5aba5e6aaa61
abumeis <37344104+abumeis@users.noreply.github.com>
2020-12-09 14:41:43 +0100

    I'm doing well so far

--- jour03.js
+++ jour03.js
@@ -7,3 +7,12 @@ console.log(fruits);
 var ingredients = ["eggs", "milk", "butter"];
 console.log(ingredients[1]);
 console.log(ingredients.indexOf("butter"));
+
+
+//03 - Add and Remove
+var objects = ["pen", "book", "lamp"];
+objects.unshift("chair");
+objects.pop("lamp");
+objects.push("laptop");
+objects.shift();
+console.log(objects);
