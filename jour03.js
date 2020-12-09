d31fdc99790f03fe7fccff8453c6b4ce250034e2
abumeis <37344104+abumeis@users.noreply.github.com>
2020-12-09 14:26:14 +0100

    I'm doing well so far

---
+++ jour03.js
@@ -0,0 +1,9 @@
+// 01 - Array
+var fruits = ["mango", "lemon", " blueberry"];
+console.log(fruits);
+
+
+//02 - Access
+var ingredients = ["eggs", "milk", "butter"];
+console.log(ingredients[1]);
+console.log(ingredients.indexOf("butter"));
