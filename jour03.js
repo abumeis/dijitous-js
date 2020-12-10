2f4bbed5065a04b0410e32605b72d3b9c0dbacbb
abumeis <37344104+abumeis@users.noreply.github.com>
2020-12-10 09:22:38 +0100

    i'm not happy with the resualt

--- jour03.js
+++ jour03.js
@@ -24,13 +24,44 @@ console.log(number.reverse());
 console.log(number.sort((a, b) => a - b));
 
 
+// 05-Boucle
+
+var total = 0;
+var limit = 10;
+for (var i = 0; i <= 10; i++) {
+    total = total + i;
+}
+console.log(total);
 
 
 
-//06 - Boucle
+//06 - Reverse
 var sentence = "Hello Konexio!";
 var reverse = "";
 for (var i = sentence.length - 1; i >= 0; i--) {
     reverse = reverse + sentence[i];
 }
 console.log(reverse);
+
+
+
+//Bonus
+
+for (i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz");
+    } else if (i % 5 == 0) {
+        console.log("buzz");
+    } else if (i % 3 == 0) {
+        console.log("fizz");
+    } else {
+        console.log(i);
+    }
+}
+//Bonus
+
+var promo4 = ["Ahmed", "Atif", "Hamada", "Allan", "Xav", "Seb", "Kitty", "Thi", "Nico", "EWA", "Bah", "Hassan", ];
+var randomName = promo4[Math.floor(Math.random() * promo4.length)];
+
+
+console.log(randomName)
