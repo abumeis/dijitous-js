90e9a1aa7cd8b505e5889aa9fa8a615725916106
abumeis <37344104+abumeis@users.noreply.github.com>
2020-12-14 13:57:41 +0100

    functions

--- jour04.js
+++ jour04.js
@@ -36,19 +36,25 @@ console.log(cat2.isCute);
 
 
 
-
-
-
-
-
-
-
-
 //03 - Even
 
 //Créez une fonction `checkIfEven` qui reçoit un paramètre `num`
 //Dans la fonction, à l'aide d'une condition, affichez "even" si `num` est pair, "odd" si il est impair
 //Appelez votre fonction plusieurs fois avec des chiffres différents comme argument pour vérifier les résultats
+
+
+function checkIfEven(num) {
+    if (num % 2 == 0)
+        console.log("even");
+    else
+        console.log("odd");
+}
+checkIfEven(17);
+console.log(checkIfEven);
+
+
+
+
 
 
 // 04 - Compare
