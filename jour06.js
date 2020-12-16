f963673acf6e697d47f53a5988f656f1ce3eee34
abumeis <37344104+abumeis@users.noreply.github.com>
2020-12-16 21:10:59 +0100

    third exe

--- jour06.js
+++ jour06.js
@@ -27,3 +27,44 @@ function calculateAge(dateDeNaissance) {
 }
 
 console.log(calculateAge("07/17/1991"));
+
+//## 03 - Validator
+
+// À l'aide du package `prompt`, créez une fonction `checkProfile` qui demande à l'utilisateur un email, un username et un mot de passe
+//La fonction vérifiera que l'utilisateur a saisi des informations valides (regardez la doc, et plus precisement les `pattern`) :
+// l'email doit être au bon format
+// le username ne doit comporter que des lettres, chiffres et tirets (les petits)
+// le mot de passe doit contenir au moins 6 caractères, au moins une lettre et au moins un chiffre, et peut contenir des tirets
+//Si tout est bon, elle retourne "All good !", sinon elle retourne "error"
+
+
+var prompt = require('prompt');
+
+var schema = {
+    properties: {
+        mail: {
+            pattern: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
+            message: 'Your email must have like this format forexample@gmail.com',
+            required: true
+        },
+
+        username: {
+            pattern: /^[a-z\d\-]+$/,
+            message: 'Name must be only letters, , or dashes',
+            required: true
+        },
+        password: {
+            pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\-]{6,}$/,
+            message: 'password must contain at least 6 characters, at least one letter and at least one number',
+            hidden: true
+        }
+
+    }
+};
+
+prompt.start();
+prompt.get(schema, function(err, result) {
+    console.log('Command-line input received:');
+    console.log('  name: ' + result.name);
+    console.log('  password: ' + result.password);
+});
