32803de0bb22ef9e1dd458b65c6bf34ed126e06e
abumeis <37344104+abumeis@users.noreply.github.com>
2020-12-17 14:58:47 +0100

    vacances exe2

--- jour07.js
+++ jour07.js
@@ -9,3 +9,34 @@ function sortLetters(str) {
     return str.split('').sort().join('');
 }
 console.log(sortLetters("konexio"));
+
+
+
+//## 02 - XOXO
+//Créez une fonction `countEach` qui reçoit une string contenant des x et des o en paramètre
+//La fonction retourne true si il y a autant de x que de o, sinon elle retourne false
+//Vérifiez que l'argument "xxxoooox" déclenche un true, et que "xox" déclenche un false
+
+function countEach(str) {
+    var lettres = str;
+    var countx = 0;
+    var counto = 0;
+
+    for (i = 0; i < lettres.length; i++) {
+        if (lettres.charAt(i) === "x") {
+            countx = countx + 1;
+
+        } else if (lettres.charAt(i) === "o") {
+            counto = counto + 1;
+
+        }
+    }
+    if (countx === counto) {
+        console.log(true)
+    } else(
+        console.log(false)
+    )
+
+
+};
+countEach("xxxoooox");
