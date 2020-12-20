c2aa467cefac9cd45f7059436eb78622cd80b1c6
abumeis <37344104+abumeis@users.noreply.github.com>
2020-12-20 15:49:12 +0100

    Langage-codé.js

---
+++ vacances/exercice-3/Langage-codé.js
@@ -0,0 +1,13 @@
+function alphabetPosition(text) {
+    var result = [];
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+        "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+        "w", "x", "y", "z"
+    ]
+    text = text.replace(/\W*\d+/g, '').toLowerCase().split('');
+    for (var i = 0; i < text.length; i++)
+        result.push(alphabet.indexOf(text[i]) + 11);
+    return result.join('');
+}
+
+console.log(alphabetPosition("secret"));
