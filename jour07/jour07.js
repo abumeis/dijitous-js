a6c15b69d2706583ac95d247f51e4ffb551fc1f0
abumeis <37344104+abumeis@users.noreply.github.com>
2021-01-04 14:10:36 +0100

    first exe

---
+++ jour07/jour07.js
@@ -0,0 +1,7 @@
+//Dans le fichier `jour07.js`, à l'aide du module `fs`, affichez le contenu de `jour07.txt` dans la console
+//Faites en sorte que chaque clé / valeur apparaisse sur une ligne séparée
+var fs = require("fs");
+fs.readFile("./jour07.txt", function(error, data) {
+    console.log(error);
+    console.log(data.toString().split('\n'));
+})
