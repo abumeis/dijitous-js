d02def3f00b7f63eecab05927b87fbc5fa3291b6
abumeis <37344104+abumeis@users.noreply.github.com>
2021-01-04 14:40:40 +0100

    sec-exe

--- jour07/jour07.js
+++ jour07/jour07.js
@@ -1,14 +1,42 @@
+//Dans le fichier `jour07.js`, à l'aide du module `fs`, affichez le contenu de `jour07.txt` dans la console
+//Faites en sorte que chaque clé / valeur apparaisse sur une ligne séparée
+var fs = require("fs");
+fs.readFile("./jour07.txt", function(error, data) {
+    console.log(error);
+    console.log(data.toString());
+    console.log(data.toString().split('\n'));
+
+})
+
 //02 - Map Double
 // Créez une variable `array` contenant un tableau qui contiendra les valeurs `1`, `2`, `3`, `4` et `5`
 //Créez une variable `double` qui appellera la méthode `.map()` pour contenir les doubles des valeurs de `array`
 //Affichez les valeurs de `double` dans la console
 
 var array = [1, 2, 3, 4, 5];
+var double = dob.map(function(element) {
+    return element * 2
+});
+console.log(double)
 
-function double(dob) {
-    return dob.map(function(element) {
+function d(double) {
+    return double.map(function(element) {
         return element * 2
     })
 }
 
-console.log(double(array))
+console.log(d(array))
+
+
+
+
+//## 03 - Map Names
+var longName = [{
+        firstName: "Jane",
+        lastName: "Doe"
+    },
+    {
+        firstName: "John",
+        lastName: "Smith"
+    }
+]
